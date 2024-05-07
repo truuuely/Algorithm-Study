6cb8c867738fe9c9709afd813fa8de4d993266a5
Moon Young Kang <kmyc7030@gmail.com>
2024-05-08 00:16:38 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 136 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String s = br.readLine();
+            if (s == null) {
+                break;
+            }
+            sb.append(s + "\n");
+        }
+        System.out.println(sb);
+    }
+}
