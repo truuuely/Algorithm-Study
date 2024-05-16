d71cc2763669346f3785662ddd601912b3dbbb9b
Moon Young Kang <kmyc7030@gmail.com>
2024-05-17 00:13:57 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? 1 : 0);
+    }
+}
