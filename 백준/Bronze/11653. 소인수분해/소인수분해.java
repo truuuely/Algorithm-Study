8c50fbae77218cdbbd02f72ef569c751f5322dbf
Moon Young Kang <kmyc7030@gmail.com>
2024-09-02 21:29:15 +0900

    [Bronze I] Title: 소인수분해, Time: 124 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,14 +1,26 @@
 import java.io.*;
 
 public class Main {
-    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        primeFactorization(n);
+//        primeFactorization(n);
+
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                sb.append(i + "\n");
+                n /= i;
+            }
+        }
+
+        if (n != 1) {
+            sb.append(n);
+        }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(sb.toString());
@@ -19,7 +31,7 @@ public class Main {
     static int primeFactorization(int n) {
         for (int i = 2; i <= n; i++) {
             if (n % i == 0) {
-                sb.append(i + "\n");
+//                sb.append(i + "\n");
                 return primeFactorization(n / i);
             }
         }
