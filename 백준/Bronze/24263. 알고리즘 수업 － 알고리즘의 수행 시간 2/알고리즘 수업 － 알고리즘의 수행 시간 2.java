77b668051d99e33c0c519bf467e4cfe93aa13e10
Moon Young Kang <kmyc7030@gmail.com>
2024-10-28 17:29:35 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 120 ms, Memory: 15780 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(n + "\n" + 1);
+        bw.flush();
+        bw.close();
+    }
+}
