ca73c16e70d50a5479809d614898bb5c50b49745
Moon Young Kang <kmyc7030@gmail.com>
2025-03-19 23:31:39 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,24 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] score = new int[n];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 시험 과목 개수 n 입력
+        // n 개의 점수 입력받아 int[n] 배열에 저장하기
+        // 저장 중에 최댓값 m 놓기
+        // 새로운 평균 (원점수 / m * 100) 계산하기
+
+        int n = Integer.parseInt(br.readLine());
+        int[] scores = new int[n];
+
         int max = -1;
-        int total = 0;
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            score[i] = Integer.parseInt(st.nextToken());
-            total += score[i];
-            if (max < score[i]) {
-                max = score[i];
+            scores[i] = Integer.parseInt(st.nextToken());
+
+            sum += scores[i];
+            if (max < scores[i]) {
+                max = scores[i];
             }
         }
-        System.out.println(total * 100.0 / max / n);
+
+        float ans = (float) sum * 100 / max / n;
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
     }
 }
