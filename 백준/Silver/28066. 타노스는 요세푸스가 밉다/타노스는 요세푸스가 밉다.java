39a66c771b6cd923af15937fd5bc552dd83c0cd6
Moon Young Kang <kmyc7030@gmail.com>
2025-06-13 17:02:21 +0900

    [Silver II] Title: 타노스는 요세푸스가 밉다, Time: 204 ms, Memory: 76764 KB -BaekjoonHub

---
+++ 백준/Silver/28066. 타노스는 요세푸스가 밉다/타노스는 요세푸스가 밉다.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            Integer poll = queue.poll();
+            int i = 2;
+            while (i++ <= K) {
+                queue.poll();
+            }
+            queue.add(poll);
+        }
+        System.out.println(queue.poll());
+    }
+}
