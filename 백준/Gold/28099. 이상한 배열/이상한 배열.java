32ed2b273f9b05258348a520e45bd3b481213eda
Moon Young Kang <kmyc7030@gmail.com>
2025-07-04 19:15:08 +0900

    [Gold III] Title: 이상한 배열, Time: 576 ms, Memory: 120796 KB -BaekjoonHub

--- 백준/Gold/28099. 이상한 배열/이상한 배열.java
+++ 백준/Gold/28099. 이상한 배열/이상한 배열.java
@@ -4,67 +4,71 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    public static final String YES = "Yes";
+    public static final String NO = "No";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine()); // 테스트케이스 20만 이하
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine()); // T * N <= 20만
-            int[] nums = new int[N];
+        for (int t = 0; t < T; t++) { // T * N <= 20만
+            int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            HashMap<Integer, Integer> countMap = new HashMap<>(); // 숫자가 몇 개 나왔는지
-            for (int j = 0; j < N; j++) {
-                nums[j] = Integer.parseInt(st.nextToken());
-                countMap.put(nums[j], countMap.getOrDefault(nums[j], 0) + 1);
+            int[] arr = new int[N];
+
+            Map<Integer, List<Integer>> indicesByValue = new HashMap<>(); // <값, 위치>
+
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+
+                indicesByValue.computeIfAbsent(arr[i], integer -> new ArrayList<>()) // 없는 경우 배열 생성 후 add
+                        .add(i);
             }
 
-            // 2개 이상 나오는 숫자만 저장하는 set
-            Set<Integer> set = new HashSet<>();
-            for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-                if (entry.getValue() > 1) {
-                    set.add(entry.getKey());
+            int[] nextGreaterIndex = getNextGreaterIndices(arr);
+            String ans = YES;
+
+            for (int i = 0; i < N; i++) {
+                List<Integer> indices = indicesByValue.get(arr[i]);
+
+                // 2개 이상 나오지 않았으면 패스
+                if (indices.size() < 2) {
+                    continue;
+                }
+
+                int oIdxOfCurNum = nextGreaterIndex[i]; // i 번째 수의 오큰수 인덱스
+
+                // i의 오큰수가 범위 내에 있으면 이상한 배열 X
+                if (oIdxOfCurNum > indices.get(0) && oIdxOfCurNum < indices.get(indices.size() - 1)) {
+                    ans = NO;
+                    break;
                 }
             }
 
-            // 각 숫자의 인덱스를 저장
-            HashMap<Integer, List<Integer>> check = new HashMap<>();
-
-            // 오큰수 위치 저장
-            int[] oIdx = new int[N];
-            Arrays.fill(oIdx, N + 1);
-
-            Stack<Integer> stack = new Stack<>();
-
-            // 오큰수 구하고 check
-            for (int j = 0; j < N; j++) {
-                // 중복 숫자면 check에 인덱스 저장
-                if (set.contains(nums[j])) {
-                    check.putIfAbsent(nums[j], new ArrayList<>());
-                    check.get(nums[j]).add(j);
-                }
-
-                // 오큰수 찾기
-                while (!stack.isEmpty() && nums[j] > nums[stack.peek()]) {
-                    oIdx[stack.pop()] = j;
-                }
-
-                stack.push(j);
-            }
-
-            String ans = "Yes";
-            for (int j = 0; j < N; j++) {
-                if (check.containsKey(nums[j])) {
-                    List<Integer> indices = check.get(nums[j]);
-
-                    if (oIdx[j] < indices.get(indices.size() - 1)) {
-                        ans = "No";
-                        break;
-                    }
-                }
-            }
-            System.out.println(ans);
+            sb.append(ans).append('\n');
         }
+
+        System.out.println(sb);
+    }
+
+    // 오큰수 인덱스 구하기
+    private static int[] getNextGreaterIndices(int[] arr) {
+        int[] res = new int[arr.length];
+        Arrays.fill(res, -1);
+
+        Stack<Integer> stack = new Stack<>(); // 오큰수를 구해야하는 값의 인덱스
+
+        for (int i = 0; i < arr.length; i++) {
+            // 오큰수를 구해야 하는 거 vs 현재 값 중 현재 값이 더 크면
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                // 오큰수를 구해야 하는 것의 오큰수는 현재 값의 인덱스
+                res[stack.pop()] = i;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
     }
 }
