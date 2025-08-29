923dbee59c933b69c1809637fd90551fac2e7c94
Moonyoung Kang <kmyc7030@gmail.com>
2025-08-29 23:20:06 +0900

    [Bronze V] Title: 오늘 날짜, Time: 168 ms, Memory: 17316 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate localDateTime = LocalDate.now();
+        System.out.println(localDateTime);
+    }
+}
