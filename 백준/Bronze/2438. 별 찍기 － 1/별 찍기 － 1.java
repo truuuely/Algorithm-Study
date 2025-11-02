4ec031f37f215f6bcfaeb43f0bada8b137da7e75
Moonyoung Kang <kmyc7030@gmail.com>
2025-11-02 23:59:01 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - i + 1; j <= N; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
