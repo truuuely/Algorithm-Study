6a82b3c4b7931248c2fb9fcb638619a8034acd29
Moonyoung Kang <kmyc7030@gmail.com>
2026-04-03 01:48:11 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,7 +1,5 @@
-select fi.id as ID, name.fish_name as FISH_NAME, fi.length as LENGTH
+select fn.fish_name as FISH_NAME, max(fi.length) as LENGTH
 from fish_info fi
-join fish_name_info name on fi.fish_type = name.fish_type
-where (fi.fish_type, fi.length) in (select fish_type, max(length) as length
-        from fish_info
-        group by fish_type)
-order by fi.id 
+join fish_name_info fn on fi.fish_type = fn.fish_type
+group by fn.fish_name
+order by fn.fish_name
