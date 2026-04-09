3efcec7969b57a61f686a3d0be27b29c9ffd3720
Moonyoung Kang <kmyc7030@gmail.com>
2026-04-09 23:41:59 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = num_list[i];
+        }
+        return arr;
+    }
+}
