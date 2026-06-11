fc282d2eda7d2e1f6ea51b76d3d2b6d96901cc1d
Moonyoung Kang <kmyc7030@gmail.com>
2026-06-11 23:10:48 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+        return answer * 1.0 / numbers.length;
+    }
+}
